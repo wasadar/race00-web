e63fada8862d549cd191cfab8377de647c22279a
Ayoub El-Haddadi <aelhaddadi@c1r10p10.khpi.ucode-connect.study>
2021-03-19 20:11:38 +0200

    changes in js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,22 @@ function Clean(){
     document.getElementById("input").innerHTML = "0";
 }
 function writeToInput(char){
-    var currentchar = document.getElementById("input").innerHTML;
+    let currentchar = document.getElementById("input").innerHTML;
     if(currentchar == "0"){
         document.getElementById("input").innerHTML = char;
-    }else{
-        document.getElementById("input").innerHTML = currentchar + char;
+    }
+    else{
+        if(isOperator(char)){
+            document.getElementById("input").innerHTML = currentchar + " " + char + " ";
+        }else{
+            document.getElementById("input").innerHTML = currentchar + char;
+        }
     }
 }
 function calculate(){
-    var egal = eval(document.getElementById('input').innerHTML);
-    document.getElementById('output').innerHTML = egal;
+    // var egal = eval(document.getElementById('input').innerHTML);
+    // document.getElementById('output').innerHTML = egal;
+    document.getElementById('output').innerHTML = get_res(document.getElementById('input').innerHTML);
 }
 let get_res = function(data){
     if (data.match(/\([^\(]*?\)/g) !== null) {
@@ -27,11 +33,15 @@ let get_res = function(data){
     let temp;
     for (let ind = 0; ind < arr.length; ind++){
         if (Number.isNaN(Number(arr[ind])) !== true) {
-            if ((minus) && (arr[ind][0] !== "-") (arr[ind][0] !== "+")) {
+            if ((minus) && (arr[ind][0] !== "-") && (arr[ind][0] !== "+")) {
                 res_arr.push("-" + arr[ind]);
                 minus = false;
             }
-            else if (minus) {
+            else if (minus && (arr[ind][0] !== "-")) {
+                res_arr.push("-" + arr[ind].slice(1));
+                minus = false;
+            }
+            else if (minus && (arr[ind][0] !== "+")) {
                 res_arr.push(arr[ind].slice(1));
                 minus = false;
             }
@@ -57,9 +67,9 @@ let get_res = function(data){
     while (check) {
         check = false;
         arr = res_arr;
-        temp = Math.max(arr.indexOf("÷"),arr.indexOf("*"));
+        temp = Math.max(arr.indexOf("/"),arr.indexOf("*"));
         if (temp !== -1){
-            if (arr[temp] === "÷"){
+            if (arr[temp] === "/"){
                 arr.splice(temp - 1,3,Number(arr[temp-1]) / Number(arr[temp+1]));
             }
             else {
@@ -68,186 +78,45 @@ let get_res = function(data){
             check = true;
         }
     }
-    temp = 0;
+    let res = 0;
     for (el of arr){
-        temp += Number(el);
+        res += Number(el);
     }
-    return temp;
-}let get_res = function(data){
-    if (data.match(/\([^\(]*?\)/g) !== null) {
-        data = data.slice(1,-1);
-    }
-    while (data.match(/\([^\(]*?\)/g) !== null){
-        data.replace(/\([^\(]*?\)/,get_res(data.match(/\([^\(]*?\)/g)[0]));
-    }
-    let arr = data.split(" ");
-    let res_arr = new Array();
-    let minus = false;
-    let temp;
-    for (let ind = 0; ind < arr.length; ind++){
-        if (Number.isNaN(Number(arr[ind])) !== true) {
-            if ((minus) && (arr[ind][0] !== "-") (arr[ind][0] !== "+")) {
-                res_arr.push("-" + arr[ind]);
-                minus = false;
-            }
-            else if (minus) {
-                res_arr.push(arr[ind].slice(1));
-                minus = false;
-            }
-            else {
-                res_arr.push(arr[ind]);
-            }
-        }
-        else {
-            if (arr[ind] === "-"){
-                if (minus){
-                    minus = false;
+    return res;
+}
+function isOperator(char){
+    if(char === "+" || char === "-" || char === "*" || char === "/"){
+        return true;
+    }else return false;
+}
+function changeSign(){
+    let current = document.getElementById('input').innerHTML;
+    if(!isNaN(current)){
+        document.getElementById('input').innerHTML = -1 * Number(current);
+    }else{
+        let arr = Array.from(current);
+        console.log(arr);
+        for (let i = arr.length-1; i >=0; i--) {
+            if(isOperator(arr[i])){
+                console.log(arr[i]);
+                if(isOperator(arr[i-3]) || isOperator(arr[i-2])){
+                    if(arr[i] === "-"){
+                        arr[i] = "+";
+                    }else arr[i] = "-"; 
+                    console.log(arr);
+                    document.getElementById('input').innerHTML = arr.join("");
+                }else{
+                    if(current.charAt(i) === "-"){
+                       let res = current.substring(0, i+1) + " + " + current.substring(i+1, current.length);
+                       document.getElementById('input').innerHTML = res; 
+                    }
+                    if(current.charAt(i) === "+"){
+                        let res = current.substring(0, i+1) + " - " + current.substring(i+1, current.length);
+                        document.getElementById('input').innerHTML = res; 
+                    }
                 }
-                else {
-                    minus = true;
-                }
-            }
-            else if (arr[ind] !== "+"){
-                res_arr.push(arr[ind]);
-            }
+                break;
+            } 
         }
     }
-    let check = true;
-    while (check) {
-        check = false;
-        arr = res_arr;
-        temp = Math.max(arr.indexOf("÷"),arr.indexOf("*"));
-        if (temp !== -1){
-            if (arr[temp] === "÷"){
-                arr.splice(temp - 1,3,Number(arr[temp-1]) / Number(arr[temp+1]));
-            }
-            else {
-                arr.splice(temp - 1,3,Number(arr[temp-1]) * Number(arr[temp+1]));
-            }
-            check = true;
-        }
-    }
-    temp = 0;
-    for (el of arr){
-        temp += Number(el);
-    }
-    return temp;
-}let get_res = function(data){
-    if (data.match(/\([^\(]*?\)/g) !== null) {
-        data = data.slice(1,-1);
-    }
-    while (data.match(/\([^\(]*?\)/g) !== null){
-        data.replace(/\([^\(]*?\)/,get_res(data.match(/\([^\(]*?\)/g)[0]));
-    }
-    let arr = data.split(" ");
-    let res_arr = new Array();
-    let minus = false;
-    let temp;
-    for (let ind = 0; ind < arr.length; ind++){
-        if (Number.isNaN(Number(arr[ind])) !== true) {
-            if ((minus) && (arr[ind][0] !== "-") (arr[ind][0] !== "+")) {
-                res_arr.push("-" + arr[ind]);
-                minus = false;
-            }
-            else if (minus) {
-                res_arr.push(arr[ind].slice(1));
-                minus = false;
-            }
-            else {
-                res_arr.push(arr[ind]);
-            }
-        }
-        else {
-            if (arr[ind] === "-"){
-                if (minus){
-                    minus = false;
-                }
-                else {
-                    minus = true;
-                }
-            }
-            else if (arr[ind] !== "+"){
-                res_arr.push(arr[ind]);
-            }
-        }
-    }
-    let check = true;
-    while (check) {
-        check = false;
-        arr = res_arr;
-        temp = Math.max(arr.indexOf("÷"),arr.indexOf("*"));
-        if (temp !== -1){
-            if (arr[temp] === "÷"){
-                arr.splice(temp - 1,3,Number(arr[temp-1]) / Number(arr[temp+1]));
-            }
-            else {
-                arr.splice(temp - 1,3,Number(arr[temp-1]) * Number(arr[temp+1]));
-            }
-            check = true;
-        }
-    }
-    temp = 0;
-    for (el of arr){
-        temp += Number(el);
-    }
-    return temp;
-}let get_res = function(data){
-    if (data.match(/\([^\(]*?\)/g) !== null) {
-        data = data.slice(1,-1);
-    }
-    while (data.match(/\([^\(]*?\)/g) !== null){
-        data.replace(/\([^\(]*?\)/,get_res(data.match(/\([^\(]*?\)/g)[0]));
-    }
-    let arr = data.split(" ");
-    let res_arr = new Array();
-    let minus = false;
-    let temp;
-    for (let ind = 0; ind < arr.length; ind++){
-        if (Number.isNaN(Number(arr[ind])) !== true) {
-            if ((minus) && (arr[ind][0] !== "-") (arr[ind][0] !== "+")) {
-                res_arr.push("-" + arr[ind]);
-                minus = false;
-            }
-            else if (minus) {
-                res_arr.push(arr[ind].slice(1));
-                minus = false;
-            }
-            else {
-                res_arr.push(arr[ind]);
-            }
-        }
-        else {
-            if (arr[ind] === "-"){
-                if (minus){
-                    minus = false;
-                }
-                else {
-                    minus = true;
-                }
-            }
-            else if (arr[ind] !== "+"){
-                res_arr.push(arr[ind]);
-            }
-        }
-    }
-    let check = true;
-    while (check) {
-        check = false;
-        arr = res_arr;
-        temp = Math.max(arr.indexOf("÷"),arr.indexOf("*"));
-        if (temp !== -1){
-            if (arr[temp] === "÷"){
-                arr.splice(temp - 1,3,Number(arr[temp-1]) / Number(arr[temp+1]));
-            }
-            else {
-                arr.splice(temp - 1,3,Number(arr[temp-1]) * Number(arr[temp+1]));
-            }
-            check = true;
-        }
-    }
-    temp = 0;
-    for (el of arr){
-        temp += Number(el);
-    }
-    return temp;
 }
